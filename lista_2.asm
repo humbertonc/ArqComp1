ba156669e7233ed10daae45a5bbb932acb444633
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2021-06-09 12:06:09 -0300

    Update lista_2.asm

--- lista_2.asm
+++ lista_2.asm
@@ -12,25 +12,19 @@ includelib \masm32\lib\masm32.lib
 includelib \masm32\lib\msvcrt.lib
 include \masm32\macros\macros.asm
 
+.data
+  a SDWORD ?
+  b SDWORD 41
+  ca SDWORD 51
 
 .code
 start:
-  mov eax, 999
-  looped:
-    cmp eax, 2000
-    je fim
-    
-    mov edx, 0
-    inc eax
-    
-    div 11
-    cmp edx, 0
-    jne looped
+  mov eax, b
+  add eax, ca
+  add eax, 100
+  mov a, eax
   
-  print:
-    printf("%d\n", count)
-    jmp looped
-    
-  fim:
-    invoke ExitProcess, 0
+  printf("A: %d", a)
+  
+  invoke ExitProcess, 0
 end start 
