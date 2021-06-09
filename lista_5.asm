3909d87c8243c83e1cb2eb14084062a7b778e990
Humberto Navarro <54050212+humbertonc@users.noreply.github.com>
2021-06-09 12:02:02 -0300

    Update lista_5.asm

--- lista_5.asm
+++ lista_5.asm
@@ -12,23 +12,24 @@ includelib \masm32\lib\masm32.lib
 includelib \masm32\lib\msvcrt.lib
 include \masm32\macros\macros.asm
 
-.data
-  a SDWORD 5
-  b SDWORD 1
-
 .code
 start:
-
-  mov eax, b
-  cmp a, eax
-  jl second
-  printf("A: %d", a)
-  jmp fim
-
- second:
-  printf("B: %d", b)   
-
- fim:
+  mov eax, 999
+  looped:
+    cmp eax, 2000
+    je fim
+    
+    mov edx, 0
+    inc eax
+    
+    div 11
+    cmp edx, 0
+    jne looped
+  
+  print:
+    printf("%d\n", count)
+    jmp looped
+    
+  fim:
     invoke ExitProcess, 0
-
 end start 
